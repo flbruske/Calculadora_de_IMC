e943973db838f7e0638bc570922d52b7425cd18e
Flavio Bruske <flaviobruske@Mac-mini-de-Flavio.local>
2020-01-21 08:32:59 -0300

    atualizacao

--- lib/main.dart
+++ lib/main.dart
@@ -26,15 +26,15 @@ class _HomeState extends State<Home> {
       double height = double.parse(heightController.text) / 100;
       double imc = weight / (height * height);
 
-      if (imc < 18.6) {
+      if (imc < 18.5) {
         _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 18.6 && imc < 24.9) {
+      } else if (imc >= 18.5 && imc < 24.9) {
         _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 24.9 && imc < 29.9) {
+      } else if (imc >= 25.0 && imc < 29.9) {
         _infoText = "Levemente acima do Peso (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 29.9 && imc < 34.9) {
+      } else if (imc >= 30.0 && imc < 34.9) {
         _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
-      } else if (imc >= 34.9 && imc < 39.9) {
+      } else if (imc >= 35.0 && imc < 39.9) {
         _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
       } else if (imc >= 40.0) {
         _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
